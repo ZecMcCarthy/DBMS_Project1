8fce9c53750f7132c641b42a890cf57351ec2c9d
Dwaine <devans2@ltu.edu>
2019-03-27 16:41:05 -0400

    Create WelcomePage.Designer.cs

---
+++ DBMSProject1/DBMSProject1/WelcomePage.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DBMSProject1
+{
+    partial class WelcomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Welcome_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Welcome_label
+            // 
+            this.Welcome_label.AutoSize = true;
+            this.Welcome_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Welcome_label.Location = new System.Drawing.Point(307, 76);
+            this.Welcome_label.Name = "Welcome_label";
+            this.Welcome_label.Size = new System.Drawing.Size(153, 38);
+            this.Welcome_label.TabIndex = 1;
+            this.Welcome_label.Text = "Welcome";
+            // 
+            // WelcomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Welcome_label);
+            this.Name = "WelcomePage";
+            this.Text = "WelcomePage";
+            this.Load += new System.EventHandler(this.WelcomePage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Welcome_label;
+    }
+}
